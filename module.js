791ab289253ef04c98b10a2a0c75424a146941c0
Kergalli <151893421+Kergalli@users.noreply.github.com>
2025-11-04 23:13:24 -0600

    initial commit
    This module adds functionality for rollable tables to whisper rolls to the GM.

---
+++ module.js
@@ -0,0 +1,25 @@
+{
+  "id": "gm-only-tables",
+  "title": "GM Only Tables",
+  "description": "Adds a checkbox to rollable tables to make rolls always whisper to GM",
+  "version": "1.0.0",
+  "compatibility": {
+    "minimum": "13",
+    "verified": "13"
+  },
+  "authors": [
+    {
+      "name": "Your Name"
+    }
+  ],
+  "esmodules": [
+    "scripts/gm-only-tables.js"
+  ],
+  "languages": [
+    {
+      "lang": "en",
+      "name": "English",
+      "path": "lang/en.json"
+    }
+  ]
+}
